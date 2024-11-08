a561bd4ccfc8ada79797a62043024d795889bef3
kunwoo park <scv74502@gmail.com>
2024-11-08 17:54:33 +0900

    init commit

---
+++ src/main/java/org/example/librarysearchjava/LibrarySearchJavaApplication.java
@@ -0,0 +1,13 @@
+package org.example.librarysearchjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrarySearchJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibrarySearchJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/librarysearchjava/LibrarySearchJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.librarysearchjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrarySearchJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
