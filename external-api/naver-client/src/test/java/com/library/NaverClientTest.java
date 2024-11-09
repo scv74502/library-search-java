6e782ee882e04651e6f604e973d326fc1925f283
kunwoo park <scv74502@gmail.com>
2024-11-10 01:49:20 +0900

    feingClient applied

--- external-api/naver-client/src/test/java/com/library/NaverClientTest.java
+++ external-api/naver-client/src/test/java/com/library/NaverClientTest.java
@@ -1,27 +1,27 @@
-package com.library;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.test.context.ActiveProfiles;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-@SpringBootTest(classes = NaverClient.class)
-@ActiveProfiles("test")
-class NaverClientTest {
-    @ComponentScan(basePackageClasses = NaverClient.class)
-    static class TestConfig{
-    }
-
-    @Autowired
-    NaverClient naverClient;
-
-    @Test
-    void callNaver(){
-        String http = naverClient.search("HTTP");
-        System.out.println(http);
-    }
-
-}
+//package com.library;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.context.annotation.ComponentScan;
+//import org.springframework.test.context.ActiveProfiles;
+//
+//import static org.junit.jupiter.api.Assertions.*;
+//
+//@SpringBootTest(classes = NaverClient.class)
+//@ActiveProfiles("test")
+//class NaverClientTest {
+//    @ComponentScan(basePackageClasses = NaverClient.class)
+//    static class TestConfig{
+//    }
+//
+//    @Autowired
+//    NaverClient naverClient;
+//
+//    @Test
+//    void callNaver(){
+//        String http = naverClient.search("HTTP");
+//        System.out.println(http);
+//    }
+//
+//}
