f1db820cb863eebd227310a7d0c2af2789d433b6
kunwoo park <scv74502@gmail.com>
2024-11-18 20:05:20 +0900

    [LS-6] db 설정, repository test 이전

---
+++ search-api/src/test/groovy/com/library/entity/DailyStatTest.groovy
@@ -0,0 +1,20 @@
+package com.library.entity
+
+import spock.lang.Specification
+
+import java.time.LocalDateTime
+
+class DailyStatTest extends Specification {
+    def "create"(){
+        given:
+        def givenQuery = "HTTP"
+        def givenLocalDateTime = LocalDateTime.of(2024, 1, 1, 1, 1, 1)
+        when:
+        def result = new DailyStat(query: givenQuery, eventDateTime: givenLocalDateTime)
+        then:
+        verifyAll(result){
+            query == givenQuery
+            eventDateTime == givenLocalDateTime
+        }
+    }
+}
